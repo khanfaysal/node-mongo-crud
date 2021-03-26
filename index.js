5188fd031cc0508e6a4c13c3011a3e14011535bc
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-03-27 01:32:07 +0600

    node-mongo-db 46 module complete

--- index.js
+++ index.js
@@ -44,19 +44,19 @@ client.connect(err => {
    productCollection.insert(product)
    .then(result => {
      console.log('data added successfully');
-     res.send('success')
+     res.redirect('/')
    })
   
   })
 
   app.patch('/update/:id', (req, res) =>{
     console.log(req.body.price);
-    productCollection.updateOne({_id: ObjectId(req.params.body)},
+    productCollection.updateOne({_id: ObjectId(req.params.id)},
     {
       $set:{ price: req.body.price, quantity: req.body.quantity}
     })
     .then(result => {
-      console.log(result);
+      res.send(result.modifiedCount > 0)
     })
   })
 
@@ -64,7 +64,7 @@ client.connect(err => {
   app.delete('/delete/:id', (req, res) =>{
     productCollection.deleteOne({_id: ObjectId(req.params.id)})
     .then( result =>{
-      console.log(result);
+      res.send(result.deletedCount > 0)
     })
   })
 });
